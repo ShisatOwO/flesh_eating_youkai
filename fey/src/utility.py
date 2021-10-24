3c4c91af49154d00378cb81dc15de9fc367b03fd
Shisa <shisatosato@gmail.com>
2021-10-24 19:16:49 +0200

    Commented fey/src/utility.py.

--- fey/src/utility.py
+++ fey/src/utility.py
@@ -1,8 +1,11 @@
 def generate_id() -> iter:
+    """infinite iterator. Starts at 0"""
     n = 0
     while True:
         yield n
         n += 1
 
 
+# That is the generator that should be used to generate the ids of the components.
+# i dont think it'll need anything more complex than simple counting
 id_generator = generate_id()
